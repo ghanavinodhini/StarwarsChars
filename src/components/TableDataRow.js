280c8f92112c14a7634fa28bcc0f2dbe98e3b22a
ghanavinodhinichandrasekaran <ghanacvino@gmail.com>
2021-05-31 13:17:44 +0200

    added modal to popup when clicked on name

--- src/components/TableDataRow.js
+++ src/components/TableDataRow.js
@@ -1,25 +1,38 @@
-import React from "react";
+import React,{useState} from "react";
 import {Link} from "react-router-dom";
+//import Modal from '../components/Modal';
+import Modal from 'react-modal';
 
 function TableDataRow({ character }) {
+  const [modalIsOpen,setmodalIsOpen] = useState(false)
     return (
         <tr className="charRowDetails">
-            <td style = {{cursor:'pointer'}} onClick={displayModal}><u>{character.name}</u></td>
+            <td style = {{cursor:'pointer'}} onClick={() => setmodalIsOpen(true)
+              }><u>{character.name}</u></td>
+            <Modal isOpen={modalIsOpen} onRequestClose={() => setmodalIsOpen(false)}>
+                <h2>{character.name}</h2>
+                <p>{character.birth_year}</p>
+                <div>
+                  <button onClick={() => setmodalIsOpen(false)}>X</button>
+                  </div>
+              </Modal>
             <td>{character.birth_year}</td>
         </tr>
     );
       };
 
-      const displayModal = props => {
-          console.log("Props" + props)
-        return (
+      const displayModal = (props) => {
+       
+        console.log("Inside displayModal" + this.props);
+       /* return (
           <div className="popup-box">
             <div className="box">
               <span className="close-icon" onClick={props.handleClose}>x</span>
               {props.content}
             </div>
           </div>
-        );
+        );*/
+        
 }
 
 export default TableDataRow;
