2e03ea68bac7df7c173a4bfb84c015e34e9e60c1
ghanavinodhinichandrasekaran <ghanacvino@gmail.com>
2021-06-01 11:49:52 +0200

    aligned modal to center of page

--- src/components/TableDataRow.js
+++ src/components/TableDataRow.js
@@ -10,39 +10,29 @@ function TableDataRow({ character }) {
             <td style = {{cursor:'pointer'}} onClick={() => setmodalIsOpen(true)
               }><u>{character.name}</u></td>
 
-            <Modal className="modal-dialog" isOpen={modalIsOpen} onRequestClose={() => setmodalIsOpen(false)}>
+            <Modal className="modal-dialog" centered isOpen={modalIsOpen} onRequestClose={() => setmodalIsOpen(false)}>
                 <div className="modal-content">
-                <div className="m-Header">
-                  <h2 className="modalHeader">{character.name}</h2>
-                  <span> <button onClick={() => setmodalIsOpen(false)}>X</button> </span>
-                </div>
+                  <div className="m-Header">
+                    <h2 className="modalHeader">{character.name}</h2>
+                    <span> <button className="closeBtn" onClick={() => setmodalIsOpen(false)}>X</button> </span>
+                  </div>
                 
-                <div className="m-Body">
-                <p className="modalBirth">Birth Year: {character.birth_year}</p>
-                <p className="modalGender">Gender: {character.gender}</p>
-                <p className="modalHeight">Height: {character.height}</p>
-                <p className="modalSkinColor">Skin Color: {character.skin_color}</p>
-                <p className="modalHairColor">Hair Color: {character.hair_color}</p>
-                <p className="modalEyeColor">Eye Color: {character.eye_color}</p>
-                </div>
+                  <div className="m-Body">
+                    <p className="modalBirth">Birth Year: {character.birth_year}</p>
+                    <p className="modalGender">Gender: {character.gender}</p>
+                    <p className="modalHeight">Height: {character.height}</p>
+                    <p className="modalSkinColor">Skin Color: {character.skin_color}</p>
+                    <p className="modalHairColor">Hair Color: {character.hair_color}</p>
+                    <p className="modalEyeColor">Eye Color: {character.eye_color}</p>
+                  </div>
                 </div>
               </Modal>
         </tr>
     );
       };
 
-      const displayModal = (props) => {
-       
-        console.log("Inside displayModal" + this.props);
-       /* return (
-          <div className="popup-box">
-            <div className="box">
-              <span className="close-icon" onClick={props.handleClose}>x</span>
-              {props.content}
-            </div>
-          </div>
-        );*/
+      
         
-}
+
 
 export default TableDataRow;
