7c4370b687f8c5e18166592118198ee02372147e
ghanavinodhinichandrasekaran <ghanacvino@gmail.com>
2021-06-02 19:06:22 +0200

    added pagination functions

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import TableHeader from "./TableHeader";
 import TableDataRow from "./TableDataRow";
+import ReactPaginate from 'react-paginate';
 
  function Table({ rows }) {
      const columns = rows[0] && Object.keys(rows[0]);
+     const [pageNum,setPageNum] = useState(0);
+        let url = rows.next;
+        console.log("url for Next:" + url);
+     
+        const async changePage = ({selected}) => {
+        setPageNum(selected);
+        let response = await  fetch(url);
+        let data = await response.json();
+        console.log("Next data fetched:"+data);
+     }
+     
     return (
         <div>
             <table cellPadding={0} cellSpacing={0}
@@ -13,6 +25,16 @@ import TableDataRow from "./TableDataRow";
                     {rows.map(person => <TableDataRow character={person} key={person.name} />)}
                 </tbody>
             </table>
+
+            <ReactPaginate 
+                previousLabel={"Prev"}
+                nextLabel={"Next"}
+                onPageChange={changePage}
+                containerClassName={"paginationBtns"}
+                previousLinkClassName={"previousBtn"}
+                nextLinkClassName={"nextBtn"}
+                disabledClassName={"paginationDisabled"}
+                activeClassName={"paginationActive"} />
         </div>
     );
 }
