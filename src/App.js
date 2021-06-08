9f18985f672350ad3f7ae980f26f0ffe3d67585f
ghanavinodhinichandrasekaran <ghanacvino@gmail.com>
2021-06-08 23:06:10 +0200

    removed comments

--- src/App.js
+++ src/App.js
@@ -2,52 +2,20 @@ import logo from './logo.svg';
 import './App.css';
  import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router,Switch,Route} from "react-router-dom";
-//import Table from "./components/Table";
-//import TableDataRow from "./components/TableDataRow";
-//import {Link} from "react-router-dom";
-//import {FaSearch} from "react-icons/fa";
 import FetchPeople from './components/FetchPeople';
 
 
 
 function App() {
-
-   /*  const [people,setPeople] = useState([]);
-    const [isFetching, setIsFetching] = useState(false);
-    const [searchCharacter, setSearchCharacter] = useState("");
-    const [nextPg, setNextPg]= useState("");
-    
-    useEffect(() => {
-        async function fetchPeople(){
-          let response = await fetch('http://swapi.dev/api/people/?format=json');
-          let data = await response.json();
-
-          setPeople(data.results);
-          setNextPg(data.next);
-        }
-        fetchPeople();
-  },[]);
-    console.log('next Page',nextPg);
-    console.log('data', people);
-
-    function Search(rows){
-        return rows.filter(person => person.name.toLowerCase().indexOf(searchCharacter) > -1)
-    
-    } */
-    
   return (
-    
     <Router>
        <div className="App">
-      
+          <header className="App-header"> Star Wars People </header>
           <Switch>         
             <Route path ="/" component={FetchPeople}/>
           </Switch>
-          
       </div>
-     </Router>
-      
-         
+     </Router>   
   );
 }
 
--- src/components/Table.js
+++ src/components/Table.js
@@ -11,96 +11,16 @@ import FetchPeople from "./FetchPeople";
      console.log("Prev value in Table:"+prevPage);
      const [pageNum,setPageNum] = useState(0);
      const [urlPage,seturlPage] = useState("");
-     //const [ tableData, setTableData] = useState(rows);
-
-       // let urlNext= nextPage;
-       // let urlPrev = "";
-    
         
-
-        /* const handleClick = () => {
-            seturlPage(nextPage);
-            console.log("inside handle click");
-            console.log("urlPage state value:"+urlPage);
-            if(nextPage!== ""){
-                console.log("Inside return");
-            
-                    // <FetchPeople url={urlPage} />
-                    async function currentTableData() {
-                        let response = await fetch(urlPage);
-                        let data = await response.json();
-                        console.log("Next data fetched in currentTableData:"+data.results);
-                        setTableData(data.results);
-                        seturlPage(data.next);
-                    }
-                    currentTableData();
-            
-            }
-        } */
-       /* const changePage = (urlNext) => {
-            console.log("Next clicked in ChangePage:"+urlNext);
-            seturlPage(urlNext);
-            <FetchPeople url={urlPage} />
-            
-        }*/
-     
-       /*  const changePage = ({selected}) => {
-             setPageNum(selected);
-             console.log("pagenum:"+selected);
-             {
-                 
-                /* rows.map((p,i)=>{
-                     urlNext = p.next;
-
-                     console.log("p value:"+p);
-                     console.log("url for Next:" + urlNext);
-                 })
-                }*/
-        /*let response = await  fetch(url);
-        let data = await response.json();
-        console.log("Next data fetched:"+data);
-     } */
-
-    /* async function changePage ({page}) {        
-        seturlPage(page);
-        console.log("pageurl:"+urlPage);
-        {
-            let response = await  fetch(urlPage);
-            let data = await response.json();
-            console.log("Next data fetched:"+data)   
-        }
-   /*let response = await  fetch(url);
-   let data = await response.json();
-   console.log("Next data fetched:"+data);
-}*/
-
-     
-     
     return (
         <div>
             <table cellPadding={0} cellSpacing={0}
-                    style={{ width: "80%", padding: 10, marginTop: 50, marginLeft:"auto", marginRight:"auto", borderCollapse:"separate", borderSpacing: 15}}>
+                    style={{ width: "fit-content", padding: 10, marginTop: 50, marginLeft:"auto", marginRight:"auto", borderCollapse:"separate", borderSpacing: 15}}>
                 <TableHeader />
                 <tbody>
                     {rows.map(person => <TableDataRow character={person} key={person.name} />)}
                 </tbody>
             </table>
-            
-                    {/* <div>
-                        <button onClick={ handleClick }> Next </button>
-                    </div> */}
-            
-           
-
-           {/*  <ReactPaginate 
-                previousLabel={"Prev"}
-                nextLabel={"Next"}
-                onPageChange={changePage}
-                // containerClassName={"paginationBtns"}
-                previousLinkClassName={"previousBtn"}
-                nextLinkClassName={"nextBtn"}
-                /* disabledClassName={"paginationDisabled"}
-                activeClassName={"paginationActive"}  /> */}
         </div>
     );
 }
